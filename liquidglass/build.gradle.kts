6a1bb916e23b7b87729e946aa79d3a9431d7fc77
dilara.kiraz <dilara.kiraz@matriksdata.com>
2025-12-24 09:27:39 +0300

    Prepare SDK release: Add Maven Publish configuration and update README with SDK usage instructions

--- liquidglass/build.gradle.kts
+++ liquidglass/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    `maven-publish`
 }
 
 android {
@@ -40,5 +41,49 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.material3)
     implementation(libs.haze)
+}
+
+// Version from gradle.properties or default
+val libraryVersion = project.findProperty("LIQUIDGLASS_VERSION") as String? ?: "1.0.0"
+
+// Publishing configuration
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.github.dilarakiraz"
+                artifactId = "liquid-glass-tabbar"
+                version = libraryVersion
+
+                from(components["release"])
+
+                pom {
+                    name.set("Liquid Glass Tab Bar")
+                    description.set("A beautiful iOS-style liquid glass tab bar for Android Jetpack Compose with blur effects and gradient borders")
+                    url.set("https://github.com/dilarakiraz/liquid-glass-tabbar")
+                    
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+                    
+                    developers {
+                        developer {
+                            id.set("dilarakiraz")
+                            name.set("Dilara Kiraz")
+                        }
+                    }
+                    
+                    scm {
+                        connection.set("scm:git:git://github.com/dilarakiraz/liquid-glass-tabbar.git")
+                        developerConnection.set("scm:git:ssh://github.com:dilarakiraz/liquid-glass-tabbar.git")
+                        url.set("https://github.com/dilarakiraz/liquid-glass-tabbar")
+                    }
+                }
+            }
+        }
+    }
 }
 
